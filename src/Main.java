753958a46eaaa07abbdbe6164181f2e64dfbe260
franciscocaero <150805792+franciscocaero@users.noreply.github.com>
2024-01-16 09:25:52 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        JFrame  frame=new JFrame("Suma de dos números");
+//        frame.setContentPane(new form1().menuPrincipal);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(640,480);
+//        frame.pack();
+//        frame.setVisible(true);
+        Scanner miVariable=new Scanner(System.in);
+        JFrame fram2=new JFrame("Calculadora");
+        fram2.setContentPane(new Calculadora().menuCalculadora);
+        fram2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fram2.setSize(640,480);
+        fram2.pack();
+        fram2.setVisible(true);
+
     }
 }
